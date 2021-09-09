d217e82fee7bb7217d050b4b4635c5bf497085ac
manunasci <nascimentomanuella73@gmail.com>
2021-09-09 18:26:55 -0300

    Adicionando o comando

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá usuario! Qual é o seu nome?");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Olá, " + nome);
+            Console.WriteLine("e seu sobrenome?");
+            string sobrenome = Console.ReadLine();
+
+            Console.Write("nome completo: ");
+            Console.WriteLine(nome + " " + sobrenome);
+            Console.Write("Nome catálogado: ");
+            Console.WriteLine($"{sobrenome.ToUpper()}" + ", "+ nome);        
+        }
+    }
+}
